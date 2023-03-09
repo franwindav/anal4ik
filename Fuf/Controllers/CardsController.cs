1e946c8e4d60e7093a6e82941538f9a105694287
franwindav <franwindav@mail.ru>
2023-03-09 22:55:31 +0300

    .

---
+++ Fuf/Controllers/CardsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fuf.Controllers
+{
+    public class CardsController : Controller
+    {
+        [Route("cards/{id}")]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Fuf/Data/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fuf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
